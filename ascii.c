62fcd50ebdd1c7bf1cc28324ec7394b81e3129ff
Jobaied Chowdhury <jobaiedchowdhuryakash031@gmail.com>
2025-06-03 14:40:53 +0600

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%[^\n]",&ch);
+    printf("%s",ch);
+    return 0;
+
+}
---
+++ o.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+long int n;
+printf("Enter a value:" );
+scanf("%ld",&n);
+printf("n=%.3ld",n);
+return 0;
+
+}
+
+
+
+
